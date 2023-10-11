3ab2469a08e83cd11bf5ba029d3d26d72443db3d
Cody Stewart <codystewart@gmail.com>
2023-10-10 19:29:16 -0500

    Update jquery_append_coaches_name.user.js
    Added the ability to make coaches names a hyperlink to discord DM related to that coach. These discord links are unique to each discord user. Using this feature requires some setup but should be pretty easy. Will a how to if needed. 
    
    Added a toggle games played button to hide games that have been played.

--- jquery_append_coaches_name.user.js
+++ jquery_append_coaches_name.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         NHL94 Online -> add coaches Name to schedule
+// @name         NHL94 Online -> add coaches Name to schedule with Discord link
 // @namespace    http://www.nhl94online.com/
-// @version      1.0
+// @version      2.0
 // @description  uses xpath to get a few tables and joins them on the coaches name based on href selectors in bla..bla..bla...
 // @author       Stewy
 // @match        https://www.nhl94online.com/html/coachpage.php*
@@ -12,30 +12,60 @@
 (function() {
     'use strict';
 
-    // Your jQuery code goes here
-
     $(document).ready(function() {
-        // Retrieve the tables using XPath
         var gamesTable = $('html body table:nth-of-type(3) tbody tr:nth-of-type(1) td:nth-of-type(2) table tbody tr:nth-of-type(2) td:nth-of-type(1) div table:nth-of-type(2) tbody tr td div table tbody tr:nth-of-type(5) td div table');
         var coachesTable = $('html body table:nth-of-type(3) tbody tr:nth-of-type(1) td:nth-of-type(1) table tbody tr td table:nth-of-type(2)');
+        // YOU MUST UPDATE THIS TABLE TO GET CLICKABLE COACHES NAMES THAT GO TO DM IN DISCORD
+        var discordUrls = {
+            'coaches_name': 'https://discord.com/channels/@me/{url_unique_to_you}',
+            'coaches_name': 'https://discord.com/channels/@me/{url_unique_to_you}'
+        };
 
-        // Iterate over each row of the games table
+        // Function to hide played games
+        function hidePlayedGames() {
+            gamesTable.find('tr').each(function(index, row) {
+                var game_played = $(row).find('td:eq(6)').text().trim();
+                if (!game_played.includes('-')) {
+                    $(row).hide();
+                }
+            });
+        }
+
         gamesTable.find('tr').each(function(index, row) {
-            // Get the away team name from column 1 and home team name from column 3
             var awayTeamCell = $(row).find('td:eq(1)');
             var homeTeamCell = $(row).find('td:eq(3)');
             var awayTeam = awayTeamCell.text().trim();
             var homeTeam = homeTeamCell.text().trim();
 
-            // Find the coach name for the away team in the coaches table
             var awayCoachName = coachesTable.find('tr:contains(' + awayTeam + ')').find('td:eq(1)').text().trim();
-            // Find the coach name for the home team in the coaches table
             var homeCoachName = coachesTable.find('tr:contains(' + homeTeam + ')').find('td:eq(1)').text().trim();
 
-            // Update the away team cell's HTML to include the coach's name
-            awayTeamCell.html(awayTeam + ' (Coach: ' + awayCoachName + ')');
-            // Update the home team cell's HTML to include the coach's name
-            homeTeamCell.html(homeTeam + ' (Coach: ' + homeCoachName + ')');
+            var awayCoachLink = discordUrls[awayCoachName] ? '<a href="' + discordUrls[awayCoachName] + '" target="_blank">' + awayCoachName + '</a>' : awayCoachName;
+            var homeCoachLink = discordUrls[homeCoachName] ? '<a href="' + discordUrls[homeCoachName] + '" target="_blank">' + homeCoachName + '</a>' : homeCoachName;
+
+            awayTeamCell.html(awayTeam + ' (Coach: ' + awayCoachLink + ')');
+            homeTeamCell.html(homeTeam + ' (Coach: ' + homeCoachLink + ')');
+        });
+
+        // Inject a button below the games table
+        var toggleButton = $('<button>').text('Toggle Played Games').css({
+            'display': 'block',
+            'margin': '10px 0',
+            'padding': '5px 10px'
+        });
+        gamesTable.after(toggleButton);
+
+        var gamesAreHidden = false;
+
+        // Toggle visibility of played games on button click
+        toggleButton.click(function() {
+            if (gamesAreHidden) {
+                gamesTable.find('tr').show();
+                gamesAreHidden = false;
+            } else {
+                hidePlayedGames();
+                gamesAreHidden = true;
+            }
         });
     });
 })();
